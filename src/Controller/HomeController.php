21e9b73cf06c948feadcb2afc9ada16bd87a02a3
Clementgrrgs <clementgarrigues@gmail.com>
2019-09-23 14:11:52 +0200

    Add navbar and style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FormController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
-     * @Route("/form", name="form")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('form/index.html.twig', [
-            'controller_name' => 'FormController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
